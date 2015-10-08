77d7b133006a384324e68eaa9805621be69e5032
Ali <saberi.ar.eng@gmail.com>
2015-10-08 02:32:00 -0400

    feat: random image
        a temporal solution to show a random image for each character

--- js/candidecontroller.js
+++ js/candidecontroller.js
@@ -1,8 +1,8 @@
 "use strict";
 (function(){
 	var candidatecontroller = function($rootScope, $routeParams){
-		var candidateId = $routeParams.candiateIndex;
-		console.log(candidateId);
+		var candiateIndex = $routeParams.candiateIndex;
+		//console.log(candidateId);
 		//console.log("routeParams is : "+ $routeParams.candiateIndex);
 		// $scope.candidateList = [
 		// {name:'Ali Saberi', overAllRate: 3, subrates:{item1:3, item2:3, item3:3, item4:3}},
@@ -10,7 +10,7 @@
 		// {name:'Sina Sheikh', overAllRate: 3, subrates:{item1:3, item2:3, item3:3, item4:3}},
 		// {name:'Mirza Abbass', overAllRate: 3, subrates:{item1:3, item2:3, item3:3, item4:3}}
 		// ];
-		$rootScope.candide = $rootScope.candidateList[candidateId];
+		$rootScope.candide = $rootScope.candidateList[parseInt(candiateIndex)];
 
 	};
 
