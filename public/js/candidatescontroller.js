ac88aa6a07529364de92671e1d9449cf2f1a0807
Ali <saberi.ar.eng@gmail.com>
2015-10-21 11:21:00 -0400

    fix: empty name candiate can not be added and modidied
        Empty names will be blocked.

--- public/js/candidatescontroller.js
+++ public/js/candidatescontroller.js
@@ -2,7 +2,9 @@
 (function(){
 	var candidatescontroller = function($rootScope, $scope, $http, candidatesFactory, $timeout){
 		var index = 0;
+		var newCandidate = {};
 		$scope.msgs = {};
+		$scope.deactiveUpdateButton = true;
 		//geting data related codes
 		var refresh = function(){candidatesFactory.get().success(function(data){
 			$rootScope.candidateList = data;
@@ -14,26 +16,30 @@
 
 		// adding a new candidate
 		$scope.addCandide = function(){
-			var newCandidate = {};
 			if ($scope.newCandidateName && $scope.newCandidateName.length)
-				{newCandidate.name = $scope.newCandidateName;}else{
-					return;
+				{
+					newCandidate.name = $scope.newCandidateName;
+					newCandidate.overAllRate = 0;
+					newCandidate.subrates = {item1 : 0, item2 : 0, item3 : 0, item4:0};
+					candidatesFactory.post(newCandidate).success(function(res){
+						refresh();
+						$scope.msgs.newCandidatesuccess =  true;
+						$timeout(function() {
+							$scope.msgs.newCandidatesuccess = false;
+						}, 2000);
+					}).error(function(err){
+						console.log('Sorry, something wrong happened when adding a new candide');
+						$scope.msgs.newCandidateError =  true;
+						$timeout(function() {
+							$scope.msgs.newCandidateError = false;
+						}, 2000);
+					});	
+				}else{
+					$scope.msgs.updateCandidateError2 =  true;
+					$timeout(function() {
+					$scope.msgs.updateCandidateError2 = false;
+				}, 2000);
 				}
-			newCandidate.overAllRate = 0;
-			newCandidate.subrates = {item1 : 0, item2 : 0, item3 : 0, item4:0};
-			candidatesFactory.post(newCandidate).success(function(res){
-				refresh();
-				$scope.msgs.newCandidatesuccess =  true;
-				$timeout(function() {
-					$scope.msgs.newCandidatesuccess = false;
-				}, 2000);
-			}).error(function(err){
-				console.log('Sorry, something wrong happened when adding a new candide');
-				$scope.msgs.newCandidateError =  true;
-				$timeout(function() {
-					$scope.msgs.newCandidateError = false;
-				}, 2000);
-			});
 		};
 
 		// removing a candiate
@@ -55,12 +61,14 @@
 
 		//editing a candidate
 		$scope.editCandide = function(id){
+			$scope.deactiveUpdateButton = false;
 			$scope.newCandidateName = $rootScope.candidateList[id].name;
 			index = id;
 		};
 
 		// update candidate name
 		$scope.update = function(){
+			if ($scope.newCandidateName && $scope.newCandidateName.length){
 			$rootScope.candidateList[index].name = $scope.newCandidateName;
 			candidatesFactory.put($rootScope.candidateList[index]._id, {name : $rootScope.candidateList[index].name}).success(function(){
 					refresh();
@@ -68,12 +76,19 @@
 					$timeout(function() {
 							$scope.msgs.updateCandidatesuccess = false;
 					}, 2000);
+					$scope.deactiveUpdateButton = true;
 			}).error(function(){
 					$scope.msgs.updateCandidateError =  true;
 					$timeout(function() {
 							$scope.msgs.updateCandidateError = false;
 					}, 2000);				
 			});
+		}else{
+				$scope.msgs.updateCandidateError2 =  true;
+					$timeout(function() {
+							$scope.msgs.updateCandidateError2 = false;
+				}, 2000);
+		}
 		};
 
 		//rating related codes
