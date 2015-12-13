066022abb827c3cca3ab4e5570ee1a502138c422
Alireza Saberi <saberi.ar.eng@gmail.com>
2015-12-13 06:10:56 -0500

    chore: watch is added
    watch is set up to watch the changes and provides subsequent tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,25 @@ module.exports = function(grunt){
 				options:{'-W083': true},
 				src:['*.js', 'routes/*.js', 'app/js/*.js']
 			} 
+		},
+		watch: {
+		  scripts: {
+		    		files: ['*.js', 'routes/*.js', 'app/js/*.js'],
+		    		tasks: ['jshint'],
+		   options: {
+		      		spawn: false,
+		    },
+		  },
 		}
+		// jasmine: {
+		// 	targert:{
+		// 		src: ['app/js/*.js', 'app/js/services/*.js']
+		// 	}
+		//  }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	// grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['jshint']);
 };
